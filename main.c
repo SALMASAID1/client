643305e996e53324e3399b0453b782f0ad922584
El Chi Taha <145617353+taha200505@users.noreply.github.com>
2024-11-07 23:14:00 +0100

    11/07 taha abbas

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<windows.h>
+#include <conio.h> 
+#include "function.h"
+int main(){
+   // add_product();
+  // delete_product();
+ // delete_product();
+   //modify_product();
+   add_supplier();
+    return 0;
+}
