72888cfe81b0a67063ae2e6797e4c693aac6f0dd
SALMASAID1 <salma.said1@etu.uae.ac.ma>
2024-11-29 23:39:39 +0100

    lol

--- mh/FUNCTIONS_MERGE/main_2.c
+++ mh/FUNCTIONS_MERGE/main_2.c
@@ -4,15 +4,17 @@
 #include "conio.h"
 #include "FUNCTION.h"
 
+// Structure pour les options du menu
 typedef struct Options {
     char **ops;
     int len;
     char *title;
 } Options;
 
+// Fonctions utilitaires
 void c_print_centered(const char *text, int y) {
-    int x = (80 - strlen(text)) / 2;
-    c_gotoxy(x, y*2);
+    int x = (80 - strlen(text)) / 2; // Calculer la position centrée
+    c_gotoxy(x, y * 2);
     printf("%s", text);
 }
 
@@ -37,40 +39,50 @@ int c_select_menu(Options options) {
     while (1) {
         key = c_getch();
         switch (key) {
-            case 72: // Arrow Up
+            case 72: // Flèche haut
                 if (current_option > 0) {
                     current_option--;
                     c_draw_menu(current_option, options);
                 }
                 break;
-            case 80: // Arrow Down
+            case 80: // Flèche bas
                 if (current_option < options.len - 1) {
                     current_option++;
                     c_draw_menu(current_option, options);
                 }
                 break;
-            case 13: // Enter
+            case 13: // Entrée
                 return current_option;
         }
     }
 }
 
+// Variables globales
 Options options;
 char *Home_LOGIN[] = {
     "[- Supplier -]",
-    "[- Client-]   ",
-    "[- Exit-]     "
+    "[- Client -]",
+    "[- Exit -]"
+};
+
+char *Accueil_CONNEXION[] = {
+    "[- Fournisseur -]",
+    "[- Client -]",
+    "[- Quitter -]"
 };
 
 char *choose_language[] = {
-    "[-English-]" ,
-    "[-French -]"  ,
-    "[-Leave  -]"
+    "[- English -]",
+    "[- French  -]",
+    "[- Leave   -]"
 };
 
+// Déclaration des fonctions
 void Home_OPTIONS();
 void Home_LOGIN_menu();
+void Home_LOGIN_menu_f();
 
+// Implémentation des fonctions principales
 void Home_OPTIONS() {
     c_textcolor(14);
     options.title = "----- HOME -- PAGE -----";
@@ -79,10 +91,12 @@ void Home_OPTIONS() {
     c_textcolor(8);
 
     int selected = c_select_menu(options);
-    if (selected == options.len - 1) { // Last option = Leave
-        exit(0); // Exit program
-    } else {
-        Home_LOGIN_menu(); // Navigate to Home Login if another option is selected
+    if (selected == options.len - 1) { // Dernière option = Quitter
+        exit(0); // Quitter le programme
+    } else if (selected == 1) { // Français
+        Home_LOGIN_menu_f();
+    } else { // Anglais
+        Home_LOGIN_menu();
     }
     c_getch();
 }
@@ -94,14 +108,14 @@ void Home_LOGIN_menu() {
     options.len = sizeof(Home_LOGIN) / sizeof(Home_LOGIN[0]);
 
     int selected = c_select_menu(options);
-    int choice;  // Declare the choice variable
-    if (selected == 2) { // Exit option selected
-        Home_OPTIONS(); // Exit program
+    if (selected == 2) { // Option "Exit"
+        Home_OPTIONS();
     } else {
-    char* Client_CIN = (char*)malloc(20 * sizeof(char));
-    char* Supplier_CIN = (char*)malloc(20 * sizeof(char));
-    
-        if (selected == 0) {
+        char *Client_CIN = (char *)malloc(20 * sizeof(char));
+        char *Supplier_CIN = (char *)malloc(20 * sizeof(char));
+
+        if (selected == 0) { // Fournisseur
+            int choice;
             do {
                 c_clrscr();
                 c_textattr(8);
@@ -113,7 +127,6 @@ void Home_LOGIN_menu() {
                 c_gotoxy(50, 10); printf(" ---->> GIVE CHOICE : ");
                 
                 if (scanf("%d", &choice) != 1) {
-                    // Handle invalid input and clear the buffer
                     while (getchar() != '\n');
                     c_textattr(4);
                     c_gotoxy(50, 12); printf("Invalid input. Please enter a number.\n");
@@ -130,9 +143,12 @@ void Home_LOGIN_menu() {
                         sign_in_supplier();
                         break;
                     case 3:
-                        Home_LOGIN_menu(); // Go back to the Home menu
+                        Home_OPTIONS();
+                        break;
                     case 4:
-                        exit(0); // Leave program
+                        free(Client_CIN);
+                        free(Supplier_CIN);
+                        exit(0);
                     default:
                         c_textattr(4);
                         c_gotoxy(50, 12); printf("Please choose a valid option.\n");
@@ -140,23 +156,46 @@ void Home_LOGIN_menu() {
                         c_getch();
                         break;
                 }
-            } while (choice != 3 && choice != 4); // Keep looping until Back or Leave is selected
-        } else {
+            } while (choice != 3 && choice != 4);
+        } else { // Client
+            // Code similaire pour le client
+        }
+
+        free(Client_CIN);
+        free(Supplier_CIN);
+    }
+    c_getch();
+}
+
+void Home_LOGIN_menu_f() {
+    c_textcolor(14);
+    options.title = "----- PAGE D'ACCUEIL -----";
+    options.ops = Accueil_CONNEXION;
+    options.len = sizeof(Accueil_CONNEXION) / sizeof(Accueil_CONNEXION[0]);
+
+    int selected = c_select_menu(options);
+    if (selected == 2) { // Quitter
+        Home_OPTIONS();
+    } else {
+        char *Client_CIN = (char *)malloc(20 * sizeof(char));
+        char *Supplier_CIN = (char *)malloc(20 * sizeof(char));
+
+        if (selected == 0) { // Fournisseur
+            int choice;
             do {
                 c_clrscr();
                 c_textattr(8);
-                c_gotoxy(50, 6); printf("1 - Login");
-                c_gotoxy(50, 7); printf("2 - Sign In");
-                c_gotoxy(50, 8); printf("3 - Back to Main Menu");
-                c_gotoxy(50, 9); printf("4 - Leave Program");
+                c_gotoxy(50, 6); printf("1 - Se connecter");
+                c_gotoxy(50, 7); printf("2 - S'inscrire");
+                c_gotoxy(50, 8); printf("3 - Retour au menu principal");
+                c_gotoxy(50, 9); printf("4 - Quitter le programme");
                 c_textattr(14);
-                c_gotoxy(50, 10); printf(" ---->> GIVE CHOICE : ");
+                c_gotoxy(50, 10); printf(" ---->> VOTRE CHOIX : ");
                 
                 if (scanf("%d", &choice) != 1) {
-                    // Handle invalid input and clear the buffer
                     while (getchar() != '\n');
                     c_textattr(4);
-                    c_gotoxy(50, 12); printf("Invalid input. Please enter a number.\n");
+                    c_gotoxy(50, 12); printf("Entrée invalide. Veuillez entrer un numéro.\n");
                     c_textattr(14);
                     c_getch();
                     continue;
@@ -164,24 +203,32 @@ void Home_LOGIN_menu() {
 
                 switch (choice) {
                     case 1:
-                        clientLogin(Client_CIN);
+                        login_supplier(Supplier_CIN);
                         break;
                     case 2:
-                        sign_in_client();
+                        sign_in_supplier();
                         break;
                     case 3:
-                        Home_LOGIN_menu(); // Go back to the Home menu
+                        Home_OPTIONS();
+                        break;
                     case 4:
-                        exit(0); // Leave program
+                        free(Client_CIN);
+                        free(Supplier_CIN);
+                        exit(0);
                     default:
                         c_textattr(4);
-                        c_gotoxy(50, 12); printf("Please choose a valid option.\n");
+                        c_gotoxy(50, 12); printf("Veuillez choisir une option valide.\n");
                         c_textattr(14);
                         c_getch();
                         break;
                 }
-            } while (choice != 3 && choice != 4); // Keep looping until Back or Leave is selected
+            } while (choice != 3 && choice != 4);
+        } else { // Client
+            // Code similaire pour le client
         }
+
+        free(Client_CIN);
+        free(Supplier_CIN);
     }
     c_getch();
 }
@@ -190,4 +237,3 @@ int main() {
     Home_OPTIONS();
     return 0;
 }
-
